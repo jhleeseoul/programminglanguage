11b53dffa10e21742dda6e586224202bacfa6f79
jhleeseoul <jhleegit@gmail.com>
2025-04-03 15:19:27 +0900

    update k.ml

--- hw-3/k__/lib/k_initial.ml
+++ hw-3/k__/lib/k_initial.ml
@@ -154,11 +154,11 @@ module K : KMINUS = struct
     | EQUAL of exp * exp
     | LESS of exp * exp
     | NOT of exp
-    | SEQ of exp * exp
-    | IF of exp * exp * exp
-    | WHILE of exp * exp
-    | LETV of id * exp * exp
-    | ASSIGN of id * exp
+    | SEQ of exp * exp  (** sequence *)
+    | IF of exp * exp * exp  (** if-then-else *)
+    | WHILE of exp * exp  (** while loop *)
+    | LETV of id * exp * exp  (** variable binding *)
+    | ASSIGN of id * exp  (** assgin to variable *)
     | READ of id
     | WRITE of exp
 
@@ -203,62 +203,9 @@ module K : KMINUS = struct
         let v, mem' = eval mem env e in
         let l = lookup_env_loc env x in
         (v, Mem.store mem' l v)
-    | NUM n -> (Num n, mem)
-    | TRUE -> (Bool true, mem)
-    | FALSE -> (Bool false, mem)
-    | UNIT -> (Unit, mem)
-    | VAR x ->
-        let l = lookup_env_loc env x in
-        let v = Mem.load mem l in
-        (v, mem)
-    | ADD (e1, e2) ->
-        let v1, mem1 = eval mem env e1 in
-        let v2, mem2 = eval mem1 env e2 in
-        (Num (value_int v1 + value_int v2), mem2)
-    | SUB (e1, e2) ->
-        let v1, mem1 = eval mem env e1 in
-        let v2, mem2 = eval mem1 env e2 in
-        (Num (value_int v1 - value_int v2), mem2)
-    | MUL (e1, e2) ->
-        let v1, mem1 = eval mem env e1 in
-        let v2, mem2 = eval mem1 env e2 in
-        (Num (value_int v1 * value_int v2), mem2)
-    | DIV (e1, e2) ->
-        let v1, mem1 = eval mem env e1 in
-        let v2, mem2 = eval mem1 env e2 in
-        let n2 = value_int v2 in
-        if n2 = 0 then raise (Error "Division by zero")
-        else (Num (value_int v1 / n2), mem2)
-    | EQUAL (e1, e2) ->
-        let v1, mem1 = eval mem env e1 in
-        let v2, mem2 = eval mem1 env e2 in
-        (Bool (v1 = v2), mem2)
-    | LESS (e1, e2) ->
-        let v1, mem1 = eval mem env e1 in
-        let v2, mem2 = eval mem1 env e2 in
-        (Bool (value_int v1 < value_int v2), mem2)
-    | NOT e ->
-        let v, mem1 = eval mem env e in
-        (Bool (not (value_bool v)), mem1)
-    | SEQ (e1, e2) ->
-        let _, mem1 = eval mem env e1 in
-        eval mem1 env e2
-    | IF (e, e1, e2) ->
-        let v_cond, mem1 = eval mem env e in
-        if value_bool v_cond then eval mem1 env e1
-        else eval mem1 env e2
-    | WHILE (cond, body) ->
-        let rec loop mem =
-          let v_cond, mem1 = eval mem env cond in
-          if value_bool v_cond then
-            let _, mem2 = eval mem1 env body in
-            loop mem2
-          else (Unit, mem1)
-        in loop mem
+    | _ -> failwith "Unimplemented" (* TODO : Implement rest of the cases *)
 
-  let run (mem, env, prog) =
-    let (v, _) = eval mem env prog in
+  let run (mem, env, pgm) =
+    let v, _ = eval mem env pgm in
     v
 end
-
-
