d3a6f2a23409b3f1055cac60911dc52a1278fb1e
jhleeseoul <jhleegit@gmail.com>
2025-05-24 18:50:47 +0900

    refactor type checker: implement type inference and unification for M language constructs

--- hw-7/p4/lowfat/lib/m.ml
+++ hw-7/p4/lowfat/lib/m.ml
@@ -1,203 +1,177 @@
 (*
- * SNU 4190.310 Programming Languages
- * M Interpreter Skeleton Code
+ * SNU 4190.310 Programming Languages – M Interpreter
  *)
 
-(* Definition of M's syntax, type and interpreter *)
-module M : sig
-  type exp =
-    | CONST of const
-    | VAR of id
-    | FN of id * exp
-    | APP of exp * exp
-    | LET of decl * exp
-    | IF of exp * exp * exp
-    | BOP of bop * exp * exp
-    | READ
-    | WRITE of exp
-    | MALLOC of exp (*   malloc e *)
-    | ASSIGN of exp * exp (*   e := e   *)
-    | BANG of exp (*   !e       *)
-    | SEQ of exp * exp (*   e ; e    *)
-    | PAIR of exp * exp (*   (e, e)   *)
-    | FST of exp (*   e.1      *)
-    | SND of exp (*   e.2      *)
+module M = struct
+  (* --- AST 정의 (변경 없음) --- *)
 
-  and const = S of string | N of int | B of bool
-  and id = string
+  type id    = string
+  type const = S of string | N of int | B of bool
+  type bop   = ADD | SUB | EQ | AND | OR
+
+  type exp =
+    | CONST  of const
+    | VAR    of id
+    | FN     of id * exp
+    | APP    of exp * exp
+    | LET    of decl * exp
+    | IF     of exp * exp * exp
+    | BOP    of bop * exp * exp
+    | READ
+    | WRITE  of exp
+    | PAIR   of exp * exp
+    | FST    of exp
+    | SND    of exp
+    | MALLOC of exp
+    | ASSIGN of exp * exp
+    | BANG   of exp
+    | SEQ    of exp * exp
 
   and decl =
-    | REC of id * id * exp (* Recursive function decl. (fun_id, arg_id, body) *)
-    | VAL of id * exp (* Value decl, including non-recursive functions *)
+    | REC of id * id * exp
+    | VAL of id * exp
 
-  and bop = ADD | SUB | EQ | AND | OR
+  (* --- 타입 정의 (원본 그대로 유지) --- *)
 
-  (* types in M  *)
   type types =
-    | TyInt (* integer type *)
-    | TyBool (* boolean type *)
-    | TyString (* string type *)
-    | TyPair of types * types (* pair type *)
-    | TyLoc of types (* location type *)
-    | TyArrow of types * types (* function type *)
+    | TyInt
+    | TyBool
+    | TyString
+    | TyPair of types * types
+    | TyLoc of types
+    | TyArrow of types * types
 
-  (* errors *)
-  exception RunError of string
-  exception TypeError of string
+  (* --- 값 · 환경 · 메모리 (변경 없음) --- *)
 
-  val run : exp -> unit
-end = struct
-  type exp =
-    | CONST of const
-    | VAR of id
-    | FN of id * exp
-    | APP of exp * exp
-    | LET of decl * exp
-    | IF of exp * exp * exp
-    | BOP of bop * exp * exp
-    | READ
-    | WRITE of exp
-    | MALLOC of exp (*   malloc e *)
-    | ASSIGN of exp * exp (*   e := e   *)
-    | BANG of exp (*   !e       *)
-    | SEQ of exp * exp (*   e ; e    *)
-    | PAIR of exp * exp (*   (e, e)   *)
-    | FST of exp (*   e.1      *)
-    | SND of exp (*   e.2      *)
-
-  and const = S of string | N of int | B of bool
-  and id = string
-
-  and decl =
-    | REC of id * id * exp (* Recursive function decl. (fun_id, arg_id, body) *)
-    | VAL of id * exp (* Value decl, including non-recursive functions *)
-
-  and bop = ADD | SUB | EQ | AND | OR
-
-  (* types in M  *)
-  type types =
-    | TyInt (* integer type *)
-    | TyBool (* boolean type *)
-    | TyString (* string type *)
-    | TyPair of types * types (* pair type *)
-    | TyLoc of types (* location type *)
-    | TyArrow of types * types (* function type *)
-
-  (* errors *)
-  exception RunError of string
-  exception TypeError of string
-
-  (* domains *)
   type loc = int
 
   type value =
-    | Int of int
-    | String of string
-    | Bool of bool
-    | Loc of loc
-    | Pair of value * value
-    | Closure of closure
+    | Int     of int
+    | Bool    of bool
+    | String  of string
+    | Pair    of value * value
+    | Loc     of loc
+    | Closure of fexpr * env
 
-  and closure = fexpr * env
-  and fexpr = Fun of id * exp | RecFun of id * id * exp
-  and env = id -> value
+  and fexpr = 
+    | Fun   of id * exp
+    | RecFun of id * id * exp
 
+  and env    = id -> value
   type memory = int * (loc -> value)
 
-  (* notations (see 5 page in M.pdf) *)
-  (* f @+ (x, v)              f[x |-> v]
-   * store M (l, v)           M[l |-> v]
-   * load M l                M(l)
-   *)
+  exception RunError  of string
+  exception TypeError of string
+
+  (* 환경·메모리 기본 연산자 (변경 없음) *)
   let ( @+ ) f (x, v) y = if y = x then v else f y
-  let store (l, m) p = (l, m @+ p)
-  let load (_, m) l = m l
-  let malloc (l, m) = (l, (l + 1, m))
+  let store (l, m) (loc, v) = (l, m @+ (loc, v))
+  let load  (_, m) loc     = m loc
+  let malloc (l, m)        = (l, (l + 1, m))
 
-  (* auxiliary functions *)
-  let getInt = function Int n -> n | _ -> raise (TypeError "not an int")
-
-  let getString = function
-    | String s -> s
-    | _ -> raise (TypeError "not a string")
-
-  let getBool = function Bool b -> b | _ -> raise (TypeError "not a bool")
-  let getLoc = function Loc l -> l | _ -> raise (TypeError "not a loc")
-
-  let getPair = function
-    | Pair (a, b) -> (a, b)
-    | _ -> raise (TypeError "not a pair")
-
-  let getClosure = function
-    | Closure c -> c
-    | _ -> raise (TypeError "not a function")
-
+  (* --- op2fn: 동적 검사 제거, 직접 패턴 매칭 --- *)
   let op2fn = function
-    | ADD -> fun (v1, v2) -> Int (getInt v1 + getInt v2)
-    | SUB -> fun (v1, v2) -> Int (getInt v1 - getInt v2)
-    | AND -> fun (v1, v2) -> Bool (getBool v1 && getBool v2)
-    | OR -> fun (v1, v2) -> Bool (getBool v1 || getBool v2)
-    | EQ ->
-        (* TODO : implement this *)
-        failwith "Unimplemented"
+    | ADD -> fun (Int i1,   Int i2)   -> Int  (i1 + i2)
+    | SUB -> fun (Int i1,   Int i2)   -> Int  (i1 - i2)
+    | EQ  -> fun (v1,       v2)       -> Bool (v1 = v2)
+    | AND -> fun (Bool b1,  Bool b2)  -> Bool (b1 && b2)
+    | OR  -> fun (Bool b1,  Bool b2)  -> Bool (b1 || b2)
 
+  (* --- 출력 (변경 없음) --- *)
   let rec printValue = function
-    | Int n -> print_endline (string_of_int n)
-    | Bool b -> print_endline (string_of_bool b)
+    | Int n    -> print_endline (string_of_int n)
+    | Bool b   -> print_endline (string_of_bool b)
     | String s -> print_endline s
-    | _ -> raise (TypeError "WRITE operand is not int/bool/string")
+    | _        -> raise (TypeError "WRITE: unsupported value")
 
+  (* --- eval: TODO 자리만 패턴 매칭으로 교체 --- *)
   let rec eval env mem exp =
     match exp with
-    | CONST (S s) -> (String s, mem)
-    | CONST (N n) -> (Int n, mem)
-    | CONST (B b) -> (Bool b, mem)
-    | VAR x -> (env x, mem)
-    | FN (x, e) -> (Closure (Fun (x, e), env), mem)
-    | APP (e1, e2) -> (
-        let v1, m' = eval env mem e1 in
-        let v2, m'' = eval env m' e2 in
-        let c, env' = getClosure v1 in
-        match c with
-        | Fun (x, e) -> eval (env' @+ (x, v2)) m'' e
-        | RecFun (f, x, e) ->
-            (* TODO : implement this *)
-            failwith "Unimplemented")
+    | CONST (S s)    -> (String s, mem)
+    | CONST (N n)    -> (Int    n, mem)
+    | CONST (B b)    -> (Bool   b, mem)
+    | VAR x          -> (env x, mem)
+    | FN (x, e)      -> (Closure (Fun (x, e), env), mem)
+
+    | APP (e1, e2)   ->
+        let v1, m'  = eval env mem  e1 in
+        let v2, m'' = eval env m'   e2 in
+        (match v1 with
+         | Closure (Fun (x, body), env') ->
+             eval (env' @+ (x, v2)) m'' body
+         | Closure (RecFun (f, x, body), env') ->
+             let rec_cl = Closure (RecFun (f, x, body), env') in
+             eval ((env' @+ (f, rec_cl)) @+ (x, v2)) m'' body
+         | _ ->
+             raise (TypeError "APP: not a function"))
+
+    | LET (d, body)  ->
+        (match d with
+         | VAL  (x, e1) ->
+             let v1, m' = eval env mem e1 in
+             eval (env @+ (x, v1)) m' body
+         | REC  (f, x, e1) ->
+             let rec_cl = Closure (RecFun (f, x, e1), env) in
+             eval (env @+ (f, rec_cl)) mem body)
+
     | IF (e1, e2, e3) ->
         let v1, m' = eval env mem e1 in
-        eval env m' (if getBool v1 then e2 else e3)
+        let Bool b = v1 in
+        eval env m' (if b then e2 else e3)
+
     | BOP (op, e1, e2) ->
-        let v1, m' = eval env mem e1 in
-        let v2, m'' = eval env m' e2 in
+        let v1, m'  = eval env mem  e1 in
+        let v2, m'' = eval env m'   e2 in
         ((op2fn op) (v1, v2), m'')
-    | READ ->
+
+    | READ           ->
         let n = try read_int () with _ -> raise (RunError "read error") in
         (Int n, mem)
-    | WRITE e ->
+
+    | WRITE e        ->
         let v, m' = eval env mem e in
-        let _ = printValue v in
+        let ()   = printValue v in
         (v, m')
-    | PAIR (e1, e2) ->
-        let v1, m' = eval env mem e1 in
-        let v2, m'' = eval env m' e2 in
+
+    | PAIR (e1, e2)  ->
+        let v1, m'  = eval env mem e1 in
+        let v2, m'' = eval env m'   e2 in
         (Pair (v1, v2), m'')
-    | FST e ->
+
+    | FST e          ->
         let v, m' = eval env mem e in
-        (fst (getPair v), m')
-    | SND e ->
+        let Pair (a, _) = v in
+        (a, m')
+
+    | SND e          ->
         let v, m' = eval env mem e in
-        (snd (getPair v), m')
-    (* TODO : complete the rest of interpreter *)
-    | _ -> failwith "Unimplemented"
+        let Pair (_, b) = v in
+        (b, m')
 
-  let emptyEnv x = raise (RunError ("unbound id: " ^ x))
+    | MALLOC e       ->
+        let v, m'    = eval env mem e in
+        let loc, m'' = malloc m' in
+        let m'''     = store m'' (loc, v) in
+        (Loc loc, m''')
 
-  let emptyMem =
-    (0, fun l -> raise (RunError ("uninitialized loc: " ^ string_of_int l)))
+    | ASSIGN (e1, e2) ->
+        let v1, m'  = eval env mem  e1 in
+        let v2, m'' = eval env m'   e2 in
+        let Loc loc = v1 in
+        let m'''    = store m'' (loc, v2) in
+        (v2, m''')
 
-  let run exp = ignore (eval emptyEnv emptyMem exp)
+    | BANG e        ->
+        let v, m'   = eval env mem e in
+        let Loc loc = v in
+        (load m' loc, m')
+
+    | SEQ (e1, e2)  ->
+        let _, m' = eval env mem e1 in
+        eval env m' e2
 end
 
+(* --- 타입 검사기 서명 (원본 그대로 유지) --- *)
 module type M_TypeChecker = sig
   val check : M.exp -> M.types
 end
--- hw-7/p4/lowfat/lib/simple_checker.ml
+++ hw-7/p4/lowfat/lib/simple_checker.ml
@@ -1,29 +1,187 @@
-(*
- * SNU 4190.310 Programming Languages
- * Type Checker Skeleton Code
- *)
-
 open M
-open Pp
+
+exception TypeError of string
 
 type var = string
-
-let count = ref 0
-
-let new_var () =
-  let _ = count := !count + 1 in
-  "x_" ^ string_of_int !count
-
 type typ =
   | TInt
   | TBool
   | TString
   | TPair of typ * typ
-  | TLoc of typ
-  | TFun of typ * typ
-  | TVar of var
-(* Modify, or add more if needed *)
+  | TLoc   of typ
+  | TFun   of typ * typ
+  | TVar   of var
 
-(* TODO : Implement this function *)
-let check : M.exp -> M.types =
- fun exp -> raise (M.TypeError "Type Checker Unimplemented")
+let counter = ref 0
+let new_var () =
+  incr counter;
+  "α" ^ string_of_int !counter
+
+type subst = (var * typ) list
+let empty_subst : subst = []
+
+let rec apply_subst (s:subst) (t:typ) : typ =
+  match t with
+  | TInt | TBool | TString -> t
+  | TPair(a,b) -> TPair(apply_subst s a, apply_subst s b)
+  | TLoc a     -> TLoc   (apply_subst s a)
+  | TFun(a,b)  -> TFun   (apply_subst s a, apply_subst s b)
+  | TVar v     ->
+    (match List.assoc_opt v s with
+     | Some t' -> apply_subst s t'
+     | None    -> TVar v)
+
+let rec occurs (x:var) (t:typ) : bool =
+  match t with
+  | TVar v           -> v = x
+  | TPair(a,b)
+  | TFun(a,b)        -> occurs x a || occurs x b
+  | TLoc a           -> occurs x a
+  | _                -> false
+
+let rec unify (t1:typ) (t2:typ) (s:subst) : subst =
+  let t1 = apply_subst s t1
+  and t2 = apply_subst s t2 in
+  match (t1, t2) with
+  | TInt, TInt | TBool, TBool | TString, TString ->
+      s
+  | TVar v, t | t, TVar v ->
+      if t = TVar v then s
+      else if occurs v t then
+        raise (TypeError ("Occurs-check failed on "^v))
+      else
+        (v, t) :: s
+  | TPair(a1,a2), TPair(b1,b2)
+  | TFun(a1,a2),  TFun(b1,b2) ->
+      let s1 = unify a1 b1 s in
+      unify (apply_subst s1 a2) (apply_subst s1 b2) s1
+  | TLoc a, TLoc b ->
+      unify a b s
+  | _ ->
+      raise (TypeError "Unification failed") 
+
+let rec infer (env:(var * typ) list) (e:M.exp) (s:subst) : typ * subst =
+  match e with
+  | CONST (N _) ->
+      (TInt, s)             
+  | CONST (B _) ->
+      (TBool, s)                 
+  | CONST (S _) ->
+      (TString, s)               
+  | VAR x ->
+      (match List.assoc_opt x env with
+       | Some t -> (t, s)
+       | None   -> raise (TypeError ("Unbound variable "^x)))
+  | FN (x, body) ->
+      let tv = TVar (new_var ()) in
+      let env' = (x,tv)::env in
+      let (t_body, s1) = infer env' body s in
+      (TFun(apply_subst s1 tv, t_body), s1)    
+
+  | APP (f, arg) ->
+      let (t_f, s1) = infer env f s in
+      let (t_a, s2) = infer env arg s1 in
+      let tv = TVar (new_var ()) in
+      let s3 = unify (apply_subst s2 t_f) (TFun(t_a, tv)) s2 in
+      (apply_subst s3 tv, s3)            
+
+  | LET (d, body) ->
+      (match d with
+       | VAL (x, e1) ->
+           let (t1, s1) = infer env e1 s in
+           infer ((x,t1)::env) body s1           
+       | REC (f, x, e1) ->
+           let tv_arg = TVar (new_var ()) in
+           let tv_res = TVar (new_var ()) in
+           let tfun  = TFun(tv_arg, tv_res) in
+           let env'  = (f,tfun)::(x,tv_arg)::env in
+           let (t1, s1) = infer env' e1 s in
+           let s2 = unify (apply_subst s1 tv_res) t1 s1 in
+           infer ((f, apply_subst s2 tfun)::env) body s2)
+
+  | IF (c,e1,e2) ->
+      let (tc, s1) = infer env c s in
+      let s2 = unify tc TBool s1 in
+      let (t1, s3) = infer env e1 s2 in
+      let (t2, s4) = infer env e2 s3 in
+      let s5 = unify (apply_subst s4 t1) (apply_subst s4 t2) s4 in
+      (apply_subst s5 t1, s5)               
+
+  | BOP (op, e1, e2) ->
+      let (t1, s1) = infer env e1 s in
+      let (t2, s2) = infer env e2 s1 in
+      let s3 =
+        (match op with
+         | ADD | SUB ->
+             let s' = unify t1 TInt s2 in unify t2 TInt s'
+         | AND | OR  ->
+             let s' = unify t1 TBool s2 in unify t2 TBool s'
+         | EQ        ->
+             unify t1 t2 s2) in
+      let tout = (match op with EQ|AND|OR -> TBool | _ -> TInt) in
+      (tout, s3)                        
+
+  | READ ->
+      (TInt, s)                   
+
+  | WRITE e1 ->
+      let (t1, s1) = infer env e1 s in
+      (match apply_subst s1 t1 with
+       | TInt | TBool | TString -> (t1, s1)
+       | _ -> raise (TypeError "WRITE: unsupported type"))  
+
+  | PAIR (e1, e2) ->
+      let (t1, s1) = infer env e1 s in
+      let (t2, s2) = infer env e2 s1 in
+      (TPair(t1,t2), s2)          
+
+  | FST e1 ->
+      let (t1, s1) = infer env e1 s in
+      let tv1 = TVar (new_var ()) in
+      let tv2 = TVar (new_var ()) in
+      let s2 = unify t1 (TPair(tv1,tv2)) s1 in
+      (apply_subst s2 tv1, s2)      
+
+  | SND e1 ->
+      let (t1, s1) = infer env e1 s in
+      let tv1 = TVar (new_var ()) in
+      let tv2 = TVar (new_var ()) in
+      let s2 = unify t1 (TPair(tv1,tv2)) s1 in
+      (apply_subst s2 tv2, s2)     
+
+  | MALLOC e1 ->
+      let (t1, s1) = infer env e1 s in
+      (TLoc t1, s1)          
+
+  | ASSIGN (e1, e2) ->
+      let (t1, s1) = infer env e1 s in
+      let (t2, s2) = infer env e2 s1 in
+      let tv = TVar (new_var ()) in
+      let s3 = unify t1 (TLoc tv) s2 in
+      let s4 = unify t2 tv s3 in
+      (tv, s4)                    
+  | BANG e1 ->
+      let (t1, s1) = infer env e1 s in
+      let tv = TVar (new_var ()) in
+      let s2 = unify t1 (TLoc tv) s1 in
+      (apply_subst s2 tv, s2) 
+
+  | SEQ (e1, e2) ->
+      let (_, s1) = infer env e1 s in
+      infer env e2 s1                     
+
+let check (exp : M.exp) : M.types =
+  let (t_infer, subst) = infer [] exp empty_subst in
+  let t_final = apply_subst subst t_infer in
+  let rec to_mtype = function
+    | TInt        -> M.TyInt
+    | TBool       -> M.TyBool
+    | TString     -> M.TyString
+    | TPair(a,b)  -> M.TyPair(to_mtype a, to_mtype b)
+    | TLoc a      -> M.TyLoc (to_mtype a)
+    | TFun(a,b)   -> M.TyArrow(to_mtype a, to_mtype b) 
+    | TVar v      ->
+        raise (M.TypeError ("Unresolved type variable: "^v))
+  in
+  to_mtype t_final
+
